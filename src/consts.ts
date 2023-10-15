422fe17365c38ea83962999dd1d691e7f56d3865
Vuanjun <victor@meepshop.com>
2023-10-15 16:00:54 +0800

    ssh used second time commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Guanjun';
+export const SITE_TITLE = 'Guanjun w/ SSH';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
